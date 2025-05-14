88631e108adbca8066a5adc1a33fb20fc93a7981
wakky-alcedo <wakky.alcedo@gmail.com>
2025-05-14 22:35:31 +0900

    [FormNotify] fix: fixes for form notify

--- FormNotify/main_form.js
+++ FormNotify/main_form.js
@@ -1,3 +1,11 @@
+/* コピー時のtodo */
+// FORM_NAME  : 何列目の情報を送るか指定（Aが1行目）
+// SEND_ITEM  : 何のフォームかを指定
+// 一度実行して，権限を承認する
+
+const FORM_NAME = "〇〇イベントアンケート"; // フォームの名前を指定
+const SEND_ITEM = [2, 3]; // 送信する項目が何列目か指定（1番目が1）何も要らなければなしでOK
+
 FormApp.getActiveForm();
 
 const onSubmit = (e) => {
@@ -9,5 +17,23 @@ const onSubmit = (e) => {
       {}
     );
   Logger.log(data);
-  sendMessageByWebhook(WEBHOOKURL_FORM, data);
+
+  // SEND_ITEMの質問名と回答を取得し，messageに格納
+  let message = `**${FORM_NAME}**`;
+
+  if (SEND_ITEM.length === 0) {
+    Logger.log("SEND_ITEMが空です。");
+    message += ` 回答がありました`;
+  } else {
+    message += `\n`;
+    SEND_ITEM.forEach((item) => {
+      const itemName = Object.keys(data)[item - 1]; // 0から始まるインデックスに変換
+      const itemValue = data[itemName];
+      if (itemValue !== "") {
+        message += `${itemName}: ${itemValue}\n`;
+      }
+    });
+  }
+
+  sendMessageByWebhook(WEBHOOKURL_FORM, message);
 };
