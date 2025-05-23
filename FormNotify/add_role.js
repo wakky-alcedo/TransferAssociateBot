d440424234911fa58978e5455de36c511f42600a
wakky-alcedo <wakky.alcedo@gmail.com>
2025-05-24 01:59:14 +0900

    [Form] refactor: 入力値を受け取る部分を関数化

--- FormNotify/add_role.js
+++ FormNotify/add_role.js
@@ -7,28 +7,12 @@ const ROLE_ID = "1357217103928758334"; // 付与するロールのIDを指定 �
 function addRoleFormForm (e) {
   const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("フォームの回答 1"); // シート名を適宜変更
   const row = e.range.getRow(); // 編集された行番号
-  const nowValues = sheet.getRange(row, 1, 1, sheet.getLastColumn()).getValues()[0]; // 編集された行の値を取得
-  let name = ""; // 氏名を初期化
-  try {
-    const nameColumn = sheet.getRange(1, 1, 1, sheet.getLastColumn()).getValues()[0].indexOf("氏名") + 1; // "氏名" の列番号を取得
-    name = nowValues[nameColumn - 1]; // 氏名を取得
-  } catch (e) {
-    Logger.log("Error: " + e.toString());
-    sendErrorMessage("Error: (フォーム)「氏名」という項目が見つかりませんでした．"); // エラーメッセージを送信
-    return; // 名前がなかったら終了
-  }
-  let studentNumber = ""; // 学籍番号を初期化
-  try {
-    const studentNumberColumn = sheet.getRange(1, 1, 1, sheet.getLastColumn()).getValues()[0].indexOf("学籍番号") + 1; // "学籍番号" の列番号を取得
-    studentNumber = nowValues[studentNumberColumn - 1]; // 学籍番号を取得
-  } catch (e) {
-    Logger.log("Error: " + e.toString());
-  }
-
+  const name = getValueFromSheet(sheet, row, "氏名"); // 氏名を取得
+  const studentNumber = getValueFromSheet(sheet, row, "学籍番号"); // 学籍番号を取得
+  const participation = getValueFromSheet(sheet, row, "参加しますか"); // 参加の値を取得
   Logger.log("name: " + name);
-
-  const participationColumn = sheet.getRange(1, 1, 1, sheet.getLastColumn()).getValues()[0].indexOf("参加しますか") + 1; // "参加" の列番号を取得
-  const participation = nowValues[participationColumn - 1]; // 参加の回答を取得
+  Logger.log("studentNumber: " + studentNumber);
+  Logger.log("participation: " + participation);
 
   // Discordのロールを付与する関数を呼び出す
   const discordId = getDataFromMemberList("Discord ID", name, studentNumber); // Discord IDを取得
@@ -43,6 +27,17 @@ function addRoleFormForm (e) {
   } else {
     Logger.log(`User ${name} not found`);
     sendErrorMessage(`User ${name} not found`); // エラーメッセージを送信
+  }
+}
+
+function getValueFromSheet(sheet, row, header) {
+  const headers = sheet.getRange(1, 1, 1, sheet.getLastColumn()).getValues()[0]; // ヘッダー行を取得
+  const columnIndex = headers.indexOf(header); // ヘッダーの列番号を取得
+  if (columnIndex !== -1) {
+    return sheet.getRange(row, columnIndex + 1).getValue(); // 値を取得
+  } else {
+    Logger.log(`Header "${header}" not found`);
+    return null; // ヘッダーが見つからなかった場合はnullを返す
   }
 }
 
